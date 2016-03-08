5801e7ef1158d1523102fa5c54c7d4124e682ca0
David Crespo <davidcrespoarroyo@gmail.com>
2016-03-08 19:26:50 +0000

    Add a new item to the db collection

--- getting-started/src/main/java/com/getinthecloud/mongoDB/MongoDBConnection.java
+++ getting-started/src/main/java/com/getinthecloud/mongoDB/MongoDBConnection.java
@@ -2,20 +2,55 @@ package com.getinthecloud.mongoDB;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import static java.util.Arrays.asList;
 
 /**
  * Created by davicres on 08/03/2016.
  */
 public class MongoDBConnection {
 
+    private final MongoDatabase db;
+
     public MongoDBConnection() {
         MongoClient mongoClient = new MongoClient();
-        MongoDatabase db = mongoClient.getDatabase("test");
+        db = mongoClient.getDatabase("test");
+    }
+
+    public void addItem() throws ParseException {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH);
+        db.getCollection("restaurants").insertOne(
+                new Document("address",
+                        new Document()
+                                .append("street", "2 Avenue")
+                                .append("zipcode", "10075")
+                                .append("building", "1480")
+                                .append("coord", asList(-73.9557413, 40.7720266)))
+                        .append("borough", "Manhattan")
+                        .append("cuisine", "Italian")
+                        .append("grades", asList(
+                                new Document()
+                                        .append("date", format.parse("2014-10-01T00:00:00Z"))
+                                        .append("grade", "A")
+                                        .append("score", 11),
+                                new Document()
+                                        .append("date", format.parse("2014-01-16T00:00:00Z"))
+                                        .append("grade", "B")
+                                        .append("score", 17)))
+                        .append("name", "Iguanna")
+                        .append("restaurant_id", "41704620"));
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         MongoDBConnection mongoDBConnection = new MongoDBConnection();
+        mongoDBConnection.addItem();
     }
 
 }
